ec00df8515c0f9ba1913e37fabba935e88639755
Mouaz Alabsawi <mouaz.alabsawi@gmail.com>
2018-05-29 23:30:26 +0200

    pep8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, jsonify,render_template,request
+from flask import Flask, redirect, url_for, jsonify, render_template, request
 from flask import session as user_session
 from flask_oauth import OAuth
 from urllib2 import Request, urlopen, URLError
@@ -8,129 +8,158 @@ from database_setup import Base, User, Category, Item
 import json
 
 # https://code.google.com/apis/console
-GOOGLE_CLIENT_ID = '784859935364-03l0ofenuodrongl54fi6kua606onf9e.apps.googleusercontent.com'
+GOOGLE_CLIENT_ID = '784859935364-03l0ofenuodrongl54fi6' \
+    'kua606onf9e.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = 'lkOxBobk10_KeRH12R63JZCz'
-REDIRECT_URI = '/oauth2callback'  # one of the Redirect URIs from Google APIs console
- 
+# one of the Redirect URIs from Google APIs console
+REDIRECT_URI = '/oauth2callback'
+
 SECRET_KEY = 'development key'
 DEBUG = True
- 
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
 oauth = OAuth()
 
 # database connection
-engine = create_engine('sqlite:///final_catalog.db',connect_args={'check_same_thread': False}, echo=True)
+engine = create_engine(
+    'sqlite:///final_catalog.db',
+    connect_args={'check_same_thread': False}, echo=True)
 Base.metadata.bind = engine
 # session creation
 DBSession = scoped_session(sessionmaker(bind=engine))
 session = DBSession()
-#print session
+# print session
 
-#followed the tutorial in https://pythonspot.com/login-to-flask-app-with-google/
+# followed the tutorial in
+# https://pythonspot.com/login-to-flask-app-with-google/
 
 google = oauth.remote_app('google',
                           base_url='https://www.google.com/accounts/',
-                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          authorize_url='https://accounts' +
+                          '.google.com/o/oauth2/auth',
                           request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
-                                                'response_type': 'code'},
-                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          request_token_params={
+                              'scope':
+                                  'https://www.googleapis' +
+                                  '.com/auth/userinfo.email',
+                              'response_type': 'code'},
+                          access_token_url='https://accounts' +
+                          '.google.com/o/oauth2/token',
                           access_token_method='POST',
-                          access_token_params={'grant_type': 'authorization_code'},
+                          access_token_params={
+                              'grant_type': 'authorization_code'},
                           consumer_key=GOOGLE_CLIENT_ID,
                           consumer_secret=GOOGLE_CLIENT_SECRET)
- 
+
+
 @app.route('/')
 def index():
     findAndCreateUser()
     categories = session.query(Category).all()
     c = session.query(Category).filter_by(id=1).one()
-    #return render_template('header.html',user_session=user_session,categories=categories, c=c)
-    return redirect(url_for('showCategory', category_id = c.id))
- 
-@app.route('/items/add',methods=[ 'POST'])
+    # return render_template(
+    # 'header.html',user_session=user_session,categories=categories, c=c)
+    return redirect(url_for('showCategory', category_id=c.id))
+
+
+@app.route('/items/add', methods=['POST'])
 def editItem():
-    #"""Check if user is logged in"""
-    #access_token = user_session.get('access_token')
+    # """Check if user is logged in"""
+    # access_token = user_session.get('access_token')
+
+    # authentication
     if user_session['user'] == -1:
         return redirect(url_for('login'))
-    
+
     if request.form['item_id']:
-        updatedItem = session.query(Item).filter_by(id=request.form['item_id']).one()
+        updatedItem = session.query(Item).filter_by(
+            id=request.form['item_id']).one()
     else:
         updatedItem = Item()
     c = session.query(Category).filter_by(id=request.form['category_id']).one()
-    u = session.query(User).filter_by(email=user_session['user']['email']).one()
-    updatedItem.name=request.form['name']
+    u = session.query(User).filter_by(
+        email=user_session['user']['email']).one()
+    updatedItem.name = request.form['name']
     updatedItem.category = c
-    updatedItem.description=request.form['description']
-    updatedItem.picture=request.form['picture']
+    updatedItem.description = request.form['description']
+    updatedItem.picture = request.form['picture']
 
+    # authorization
     if u.email != user_session['user']['email']:
-		return redirect('/login')
-    
+        return redirect('/login')
+
     if request.method != 'POST':
         session.add(item)
         session.commit()
-        return redirect(url_for('showCategory', category_id = c.id))
+        return redirect(url_for('showCategory', category_id=c.id))
     else:
-        return redirect(url_for('showCategory', category_id = c.id))
-    
-@app.route('/items/<int:item_id>/delete',methods=[ 'GET'])
+        return redirect(url_for('showCategory', category_id=c.id))
+
+
+@app.route('/items/<int:item_id>/delete', methods=['GET'])
 def deleteItem(item_id):
-    #"""Check if user is logged in"""
-    #access_token = user_session.get('access_token')
+    # """Check if user is logged in"""
+    # access_token = user_session.get('access_token')
+    # authentication
     if user_session['user'] == -1:
         return redirect(url_for('login'))
-    item = session.query(Item).filter_by(id = item_id).first()
-    
-    user = session.query(User).filter_by(email=user_session['user']['email']).first()
+    item = session.query(Item).filter_by(id=item_id).first()
 
+    user = session.query(User).filter_by(
+        email=user_session['user']['email']).first()
+
+    # authorization
     if user.email != user_session['user']['email']:
-		return redirect('/login')
+        return redirect('/login')
 
     session.delete(item)
     session.commit()
-    return redirect(url_for('showCategory', category_id = item.category_id))
-    
+    return redirect(url_for('showCategory', category_id=item.category_id))
+
+
 @app.route('/showCategory')
 def showCategory():
     findAndCreateUser()
     categories = session.query(Category).all()
-    category_id = id=request.args.get('category_id')
+    category_id = request.args.get('category_id')
     le_category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id).all()
     if 'user' not in user_session:
-       user_session['user'] = -1 
-    return render_template('header.html', items=items,
-    user_session=user_session,c=le_category,
-    categories=categories)
-	
-	
+        user_session['user'] = -1
+    return render_template(
+        'header.html',
+        items=items,
+        user_session=user_session, c=le_category,
+        categories=categories
+        )
+
+
 @app.route('/login')
 def login():
-	access_token = user_session.get('access_token')
-	if access_token is not None:
-		return redirect(url_for('showCategory', category_id = 1))
-	callback=url_for('authorized', _external=True)
-	return google.authorize(callback=callback)
- 
+    access_token = user_session.get('access_token')
+    if access_token is not None:
+        return redirect(url_for('showCategory', category_id=1))
+    callback = url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+
 @app.route('/logout')
 def logout():
-	user_session.clear()
-	return redirect(url_for('showCategory', category_id = 1))
+    user_session.clear()
+    return redirect(url_for('showCategory', category_id=1))
 
-#===================
+# ===================
 # JSON End Point
-#===================
+# ===================
+
 
 @app.route('/api/catalog.json')
 def showAllCatalogJSON():
     """Returns the whole catalog in json"""
     items = session.query(Item).order_by(Item.category_id)
     return jsonify(Items=[i.serialize for i in items])
+
 
 @app.route(
     '/api/item/<int:item_id>.json')
@@ -140,6 +169,7 @@ def catalogItemJSON(item_id):
         Item).filter_by(id=item_id).one()
     return jsonify(Item=item.serialize)
 
+
 @app.route(
     '/api/category/<int:category_id>.json')
 def catalogCategoryJSON(category_id):
@@ -148,12 +178,13 @@ def catalogCategoryJSON(category_id):
         Item).filter_by(id=category_id).one()
     return jsonify(Category=category.serialize)
 
+
 @app.route(
     '/api/category/<int:category_id>/items.json')
 def catalogCategoryItemsJSON(category_id):
     """Returns Items in a certain category in json"""
     items = session.query(
-        Item).filter_by(id=category_id).one()
+        Item).filter_by(category_id=category_id).all()
     return jsonify(Items=[i.serialize for i in items])
 
 
@@ -164,32 +195,37 @@ def categoriesJSON():
     return jsonify(Categories=[c.serialize for c in categories])
 
 
-	
 @app.route(REDIRECT_URI)
 @google.authorized_handler
 def authorized(resp):
     access_token = resp['access_token']
     user_session['access_token'] = access_token, ''
     return redirect(url_for('index'))
- 
- 
+
+
 @google.tokengetter
 def get_access_token():
     return user_session.get('access_token')
- 
- 
+
+
 def main():
     app.debug = True
     app.secret_key = 'super_secret_key'
-    app.run(host = '0.0.0.0', port = 5000)
- 
- #helper
+    app.run(host='0.0.0.0', port=5000)
+
+# helper
+
+
 def findAndCreateUser():
+
     if 'access_token' in user_session:
         access_token = user_session.get('access_token')[0]
         headers = {'Authorization': 'OAuth '+access_token}
-        req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
-                  None, headers)
+        req = Request(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            None,
+            headers
+            )
         try:
             res = urlopen(req)
         except URLError, e:
@@ -198,12 +234,17 @@ def findAndCreateUser():
                 user_session.pop('access_token', None)
                 return redirect(url_for('login'))
             return res.read()
-     
+
         user_session['user'] = json.load(res)
         # chek for an existing user
-        user = session.query(User).filter_by(email=user_session['user']['email']).first()
+        user = session.query(User).filter_by(
+            email=user_session['user']['email']).first()
         if user is None:
-            newUser = User(name=user_session['user']['name'], email=user_session['user']['email'], picture=user_session['user']['picture'])
+            newUser = User(
+                name=user_session['user']['name'],
+                email=user_session['user']['email'],
+                picture=user_session['user']['picture']
+                )
             session.add(newUser)
             session.commit()
 
--- database_setup.py
+++ database_setup.py
@@ -3,35 +3,38 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
-#===================
+# ===================
 # Database setup with user, catalog & item table
-# The below script creates a .db file including the schema of the item catalog project
-#===================
+# The below script creates a
+# .db file including the schema of the item catalog project
+# ===================
 
 Base = declarative_base()
 
-#User Table
+
+# User Table
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(256), nullable = False)
-    email = Column(String(256), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False)
+    email = Column(String(256), nullable=False)
     picture = Column(String(512))
-    
+
     @property
     def serialize(self):
         """Serializable User Instances"""
         return {
-            'name'          : self.name,
-            'id'            : self.id
+            'name': self.name,
+            'id': self.id
         }
+
 
 class Category(Base):
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(256), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref="category")
 
@@ -39,9 +42,10 @@ class Category(Base):
     def serialize(self):
         """Serializable Category Instances"""
         return {
-            'name'          : self.name,
-            'id'            : self.id
+            'name': self.name,
+            'id': self.id
         }
+
 
 class Item(Base):
     __tablename__ = 'item'
@@ -51,7 +55,10 @@ class Item(Base):
     description = Column(String(256))
     picture = Column(String(512))
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category, backref=backref('item', cascade='all, delete'))
+    category = relationship(
+        Category,
+        backref=backref('item', cascade='all, delete')
+        )
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref="item")
 
@@ -59,14 +66,14 @@ class Item(Base):
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-            'name'          : self.name,
-            'id'            : self.id,
-            'description'   : self.description,
-            'picture'       : self.picture,
-            'category'      : self.category.name
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'picture': self.picture,
+            'category': self.category.name
         }
 
 
-#engine = create_engine('sqlite:///final_catalog.db')
+# engine = create_engine('sqlite:///final_catalog.db')
 
-#Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
